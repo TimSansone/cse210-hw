85b2cc223b399458c5d7a221984d47ead5c6fe7a
Tim Sansone <san21048@byui.edu>
2024-04-25 03:54:47 -0400

    Week 1 - Prep 1 & 2 Assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        first_name = char.ToUpper(first_name[0]) + first_name.Substring(1);
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        last_name = char.ToUpper(last_name[0]) + last_name.Substring(1);
+
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,56 @@
 using System;
 
-class Program
+namespace prep2
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("What is your grade percentage? ");
+            string percentage = Console.ReadLine();
+            int grade_percent = int.Parse(percentage);
+            string letter_grade = "";
+            string sign = "";
+            int last_digit = grade_percent % 10;
+
+            if (last_digit <= 3)
+            {
+                sign = "-";
+            }
+            else if (last_digit >= 7)
+            {
+                sign = "+";
+            }
+
+            if (grade_percent >= 90)
+            {
+                letter_grade = "A";
+                if (last_digit >= 7)
+                {
+                    sign = "";
+                }
+            }
+            else if (grade_percent >= 80)
+            {
+                letter_grade = "B";
+            }
+            else if (grade_percent >= 70)
+            {
+                letter_grade = "C";
+            }
+            else if (grade_percent >= 60)
+            {
+                letter_grade = "D";
+            }
+            else
+            {
+                letter_grade = "F";
+                sign = "";
+            }
+
+            Console.WriteLine($"Your letter grade is: {letter_grade}{sign}");
+        }
     }
 }
