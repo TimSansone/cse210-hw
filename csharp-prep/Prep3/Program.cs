97dba07db45557df73383c63c8dac830795d396f
Tim Sansone <san21048@byui.edu>
2024-04-28 19:42:30 -0400

    Week 2 Prepare Assignments Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Data;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Clear();
+        // Console.Write("What is your magic number? ");
+        // int magic_number = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        int guess_number = -1;
+        int num_guesses = 0;
+
+        while (guess_number != magic_number)
+        {
+            Console.Write("What is your guess? (between 1 and 100) ");
+            guess_number = int.Parse(Console.ReadLine());
+            num_guesses++;
+            if (magic_number > guess_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {num_guesses} tries!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        // Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int new_num = -1;
+        while (new_num != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string user_num = Console.ReadLine();
+            new_num = int.Parse(user_num);
+            if (new_num != 0)
+            {
+                numbers.Add(new_num);
+            }
+        }
+
+        // Calculating sum of list
+        int list_sum = 0;
+        foreach (int num in numbers)
+        {
+            list_sum += num;    
+        }
+        Console.WriteLine($"The sum is: {list_sum}");
+
+        // Calculating average
+        float average = ((float)list_sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Calculating max number in the list
+        int max_num = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > max_num)
+            {
+                max_num = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max_num}");
     }
 }
