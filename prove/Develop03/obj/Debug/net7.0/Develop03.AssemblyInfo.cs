1cf0973f7e57d0b0a34c99239e1872f9863a4b5c
Tim Sansone <san21048@byui.edu>
2024-05-30 18:22:59 -0400

    Updates

--- prove/Develop03/obj/Debug/net7.0/Develop03.AssemblyInfo.cs
+++ prove/Develop03/obj/Debug/net7.0/Develop03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Develop03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b904876169eb9a8332bcb70be74081c5757e18fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01030bdddbbad364313ef5d18fe375ba152b6ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Develop03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Develop03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
